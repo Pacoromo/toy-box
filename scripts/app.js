c1bd2d4dd4be73bff1a93dec0f959603d27e0334
pacoromo <pacoromo@me.com>
2022-09-24 18:03:58 -0400

    names validated

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,76 @@
 //**Elements selectors **//
 
 //Slide Menu
-const menuButton = document.querySelector(".menuIcon");
+const menuBtn = document.querySelector(".menuIcon");
 const slideMenu = document.querySelector(".slideOutNav");
-const closeNav = document.querySelector(".hideSideNav");
+const closeNavBtn = document.querySelector(".hideSideNav");
 
-//form elements 
-const form = document.querySelector('form')
-const firstNameInput = document.getElementById('firstName');
-const LastNameInput = document.getElementById('lastName');
-const emailInput = document.getElementById('email');
-const phoneInput = document.getElementById('phone');
-const contactReason = document.getElementById('reason');
-const message = document.getElementById('message');
-const agreedTerms = document.getElementById('terms');
+//form elements
+const form = document.querySelector("form");
 
+const firstNameInput = document.getElementById("firstName");
 
+const lastNameInput = document.getElementById("lastName");
 
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("emailError");
 
+const phoneInput = document.getElementById("phone");
+const phoneError = document.getElementById("phoneError");
+
+const contactReason = document.getElementById("reason");
+const reasonPlaceholder = document.getElementById("reasonPlaceholder");
+
+const message = document.getElementById("message");
+const messagePlaceholder = document.getElementById("messagePlaceholder");
+
+const agreedTerms = document.getElementById("terms");
+const termsError = document.getElementById("termsError");
+
+//Functions
+
+const validateName = (input) => {
+  //trim whitespace
+  const name = input.value.trim();
+  let inputError;
+  //Check if first or last name
+  if (input === firstNameInput) {
+    inputError = document.getElementById("firstNameError");
+  } else {
+    inputError = document.getElementById("lastNameError");
+  }
+
+  //Check input value
+
+  if (name.length === 0) {
+    inputError.textContent = "Not Valid";
+    return false;
+  }
+  console.log(name);
+  inputError.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+  return true;
+};
 
 //** Event Listeners **//
 
 //Slide Menu
-menuButton.addEventListener("click", function () {
+menuBtn.addEventListener("click", () => {
   slideMenu.classList.add("showNav");
 });
 
-closeNav.addEventListener("click", function () {
+closeNavBtn.addEventListener("click", () => {
   slideMenu.classList.remove("showNav");
 });
 
-//Form 
+//Form elements
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-
-
+firstNameInput.addEventListener("keyup", () => {
+  validateName(firstNameInput);
+});
+lastNameInput.addEventListener("keyup", () => {
+  validateName(lastNameInput);
+});
