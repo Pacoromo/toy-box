c7fa9e775bb465e7399dcb06020b61008f48fac0
pacoromo <pacoromo@me.com>
2022-09-24 12:26:02 -0400

    containers for input groups

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,21 @@ const menuButton = document.querySelector(".menuIcon");
 const slideMenu = document.querySelector(".slideOutNav");
 const closeNav = document.querySelector(".hideSideNav");
 
-//** Event Listeners **//
+//form elements 
+const form = document.querySelector('form')
+const firstNameInput = document.getElementById('firstName');
+const LastNameInput = document.getElementById('lastName');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const contactReason = document.getElementById('reason');
+const message = document.getElementById('message');
+const agreedTerms = document.getElementById('terms');
 
+
+
+
+
+//** Event Listeners **//
 
 //Slide Menu
 menuButton.addEventListener("click", function () {
@@ -18,5 +31,7 @@ closeNav.addEventListener("click", function () {
 });
 
 //Form 
+
+
 
 
